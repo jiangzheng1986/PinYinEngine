d661ae2c2de7afdf1622c18f202c0d92b9da1bae
andersjiang <andersjiang@tencent.com>
2022-09-08 11:02:56 +0800

    first commit

---
+++ PinYinEngine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PinYinEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Chinese = "十年磨一剑。";
+            string PinYin = PinYinEngine.GetPinYin(Chinese, "_");
+            Console.WriteLine("{0} => {1}", Chinese, PinYin);
+            Console.ReadLine();
+        }
+    }
+}
